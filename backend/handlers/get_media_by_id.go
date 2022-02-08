a4c9980e34f846e6d03cd5e1802ccf7a38479afb
antongulikov <contest.gulikov@gmail.com>
2022-02-08 20:35:28 +0100

    Return uri to image.

--- backend/handlers/get_media_by_id.go
+++ backend/handlers/get_media_by_id.go
@@ -24,5 +24,5 @@ func DownloadFile(object string) []byte {
 
 func GetMediaById(c *gin.Context) {
 	media_id := c.Param("media_id")
-	c.String(http.StatusOK, "%s", b64.StdEncoding.EncodeToString(DownloadFile(media_id)))
+	c.String(http.StatusOK, "https://storage.cloud.google.com/feedgram-images/blobs/%s", media_id)
 }
