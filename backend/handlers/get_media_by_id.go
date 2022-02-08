8d569014d6daedc750fa3c779146071b81ffc55e
antongulikov <contest.gulikov@gmail.com>
2022-02-08 21:21:04 +0100

    Remove unused imports

--- backend/handlers/get_media_by_id.go
+++ backend/handlers/get_media_by_id.go
@@ -3,24 +3,7 @@ package handlers
 import (
 	"net/http"
 	"github.com/gin-gonic/gin"
-	"context"
-	"time"
-	"io/ioutil"
-	"FeedGram/clients"
-	b64 "encoding/base64"
 )
-
-func DownloadFile(object string) []byte {
-	ctx := context.Background()
-
-	ctx, cancel := context.WithTimeout(ctx, time.Second*50)
-	defer cancel()
-
-	rc, _ := clients.Uploader.Cl.Bucket(clients.Uploader.BucketName).Object(clients.Uploader.UploadPath + object).NewReader(ctx)
-	defer rc.Close()
-	slurp, _ := ioutil.ReadAll(rc)
-	return slurp;
-}
 
 func GetMediaById(c *gin.Context) {
 	media_id := c.Param("media_id")
