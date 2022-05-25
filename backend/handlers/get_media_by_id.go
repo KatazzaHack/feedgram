1545df5c61ef4da17a5fa74bb17fdf14d6ef0635
antongulikov <contest.gulikov@gmail.com>
2022-05-25 22:56:03 +0200

    path escape

--- backend/handlers/get_media_by_id.go
+++ backend/handlers/get_media_by_id.go
@@ -11,6 +11,6 @@ func GetMediaById(c *gin.Context) {
 	media_id := c.Param("media_id")
 	u, _ := utils.GenerateV4GetObjectSignedURL("blobs/" + media_id)
 	c.JSON(http.StatusOK, gin.H {
-		"id": url.QueryEscape(u),
+		"id": url.PathEscape(u),
 		});
 }
