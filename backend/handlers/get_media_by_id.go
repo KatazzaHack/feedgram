161731b10adb7b8d40038e7ea49e79254b6e87fa
antongulikov <contest.gulikov@gmail.com>
2022-05-25 22:59:26 +0200

    path escape

--- backend/handlers/get_media_by_id.go
+++ backend/handlers/get_media_by_id.go
@@ -2,7 +2,6 @@ package handlers
 
 import (
 	"net/http"
-  "net/url"
 	"github.com/gin-gonic/gin"
 	"FeedGram/utils"
 )
@@ -11,6 +10,6 @@ func GetMediaById(c *gin.Context) {
 	media_id := c.Param("media_id")
 	u, _ := utils.GenerateV4GetObjectSignedURL("blobs/" + media_id)
 	c.JSON(http.StatusOK, gin.H {
-		"id": url.PathEscape(u),
+		"id": u,
 		});
 }
