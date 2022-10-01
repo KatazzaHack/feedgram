b1f9ac056ea1808210b4d890096f7097a2f2aa47
antongulikov <contest.gulikov@gmail.com>
2022-10-01 21:41:54 +0200

    handle right data

--- backend/cmd/main/main.go
+++ backend/cmd/main/main.go
@@ -16,7 +16,7 @@ func CORSMiddleware() gin.HandlerFunc {
         c.Header("Access-Control-Allow-Credentials", "true")
         c.Header("Access-Control-Allow-Headers", "*")
         c.Header("Access-Control-Request-Headers", "*")
-        c.Header("Access-Control-Allow-Methods", "POST,HEAD,PATCH, OPTIONS, GET, PUT")
+        c.Header("Access-Control-Allow-Methods", "POST, HEAD, PATCH, OPTIONS, GET, PUT")
 
         if c.Request.Method == "OPTIONS" {
             c.AbortWithStatus(204)
