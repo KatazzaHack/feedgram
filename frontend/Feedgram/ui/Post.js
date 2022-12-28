1df1df9f80e954da3837f88d3b72ea5865d2e4de
Olga Kalitova <nimloth693@gmail.com>
2022-12-28 19:10:51 +0100

    Adding a proper Post component

--- frontend/Feedgram/ui/Post.js
+++ frontend/Feedgram/ui/Post.js
@@ -6,7 +6,7 @@ import {
   Text,
   Dimensions,
 } from 'react-native'
-import Icon from 'react-native-vector-icons';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const deviceWidth = Dimensions.get('window').width;
 
@@ -15,27 +15,63 @@ const styles = StyleSheet.create({
        padding: 10,
        width: deviceWidth,
        height: deviceWidth,
+       backgroundColor: "#FFF",
     },
     image: {
         flex: 1,
     },
+    postHeader: {
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "flex-start",
+        padding: 5,
+        width: deviceWidth,
+        backgroundColor: "#FF00000",
+    },
+    titleText: {
+        fontSize: 24,
+        paddingLeft: 10,
+    },
+    bookmarkButton: {
+        size: 24,
+        backgroundColor: "#FFF",
+        justifySelf: "flex-end",
+    },
+    ratingContainer: {
+        width: "100%",
+        height: 24,
+        backgroundColor: "#DDDDDD",
+    },
+    rating: {
+       backgroundColor: "#00FF00",
+       width: 100,
+       height: 24,
+    }
 });
 
 const Post = (props): Node => {
     imageUri = props.image_uri;
 //    contentType = props.content_type;
 //    titleText = props.title_text;
-    contentType = "";
-    titleText = props.title_text;
+    contentType = "video";
+    titleText = "Catch me If you can";
     console.log(imageUri);
     return (
       <View style={styles.container}>
-        <Icon name={content_type}
-           color="black"
-           size={24}/>
-        <Text style={styles.titleText}>
-          {titleText}
-        </Text>
+        <View style={styles.postHeader}>
+            <Icon name={contentType}
+               color="black"
+               size={24}/>
+            <Text style={styles.titleText}>
+              {titleText}
+            </Text>
+            <Icon.Button color="black"
+                style={styles.bookmarkButton} name="book-outline"/>
+        </View>
+        <View style={styles.ratingContainer}>
+            <View style={styles.rating}/>
+        </View>
         <Image style={styles.image} source={{uri: imageUri}}/>
       </View>
     );
